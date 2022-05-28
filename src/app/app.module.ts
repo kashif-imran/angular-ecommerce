9692af897100855dd20a4b4255500b2ce5fd30aa
kashif.emran@gmail.com <kashif.emran@gmail.com>
2022-05-28 23:12:55 +0530

    implement login/ logout using Okta Authenticatin provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,40 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HttpClientModule } from '@angular/common/http';
 import { ProductService } from './services/product.service';
 
-import { Routes, RouterModule} from '@angular/router';
+import { Routes, RouterModule, Router} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
-import { CartService } from './services/cart.service';
 import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 import { CheckoutComponent } from './components/checkout/checkout.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Luv2ShopFormService } from './services/luv2-shop-form.service';
-import { CheckoutService } from './services/checkout.service';
+import { LoginComponent } from './components/login/login.component';
+import { LoginStatusComponent } from './components/login-status/login-status.component';
+
+import {
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent
+} from '@okta/okta-angular';
+
+import myAppConfig from './config/my-app-config';
+
+const oktaConfig = Object.assign({
+  onAuthRequired: (injector) => {
+    const router = injector.get(Router);
+
+    // Redirect the user to your custom login page
+    router.navigate(['/login']);
+  }
+}, myAppConfig.oidc);
 
 const routes: Routes = [
+  {path: 'login/callback', component: OktaCallbackComponent},
+  {path: 'login', component: LoginComponent},
+
   {path: 'checkout', component: CheckoutComponent},
   {path: 'cart-details', component: CartDetailsComponent},
   {path: 'products/:id', component: ProductDetailsComponent},
@@ -41,16 +60,19 @@ const routes: Routes = [
     ProductDetailsComponent,
     CartStatusComponent,
     CartDetailsComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    LoginComponent,
+    LoginStatusComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule,
     NgbModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    OktaAuthModule
   ],
-  providers: [ProductService, CartService, Luv2ShopFormService, CheckoutService],
+  providers: [ProductService, { provide: OKTA_CONFIG, useValue: oktaConfig }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
+import * as OktaSignIn from '@okta/okta-signin-widget';
+
+import myAppConfig from '../../config/my-app-config';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  oktaSignin: any;
+
+  //using   "@okta/okta-angular": "^3.0.1", @okta/okta-signin-widget": "^5.2.0",
+  // later upgrade to okta latest
+  //https://stackoverflow.com/questions/69334481/okta-component-giving-error-at-runtime-cannot-read-properties-of-undefined-read
+  //https://github.com/okta/okta-angular/blob/master/MIGRATING.md
+  constructor(private oktaAuthService: OktaAuthService) {
+
+    this.oktaSignin = new OktaSignIn({
+      logo: 'assets/images/logo.png',
+      baseUrl: myAppConfig.oidc.issuer.split('/oauth2')[0],
+      clientId: myAppConfig.oidc.clientId,
+      redirectUri: myAppConfig.oidc.redirectUri,
+      authParams: {
+        pkce: true,
+        issuer: myAppConfig.oidc.issuer,
+        scopes: myAppConfig.oidc.scopes
+      }
+    });
+
+  }
+
+  ngOnInit(): void {
+    this.oktaSignin.remove();
+
+    this.oktaSignin.renderEl({
+      el: '#okta-sign-in-widget'}, // this name should be same as div tag id in login.component.html
+      (response) => {
+        if (response.status === 'SUCCESS') {
+          this.oktaAuthService.signInWithRedirect();
+        }
+      },
+      (error) => {
+        throw error;
+      }
+    );
+  }
+
+}
