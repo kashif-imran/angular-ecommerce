a4d7354e1ee0e47397549b2c91be5f850b7b9227
kashif.emran@gmail.com <kashif.emran@gmail.com>
2022-05-30 22:46:36 +0530

    HTTPS implemented in front end using openSSL certificate in live app we will use certificate provided by AWS or other cloud/ domain hosting provider
    use command npm start to start app in https mode after this

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
   oidc: {
     clientId: '0oa57nenlnYTXHJ7m5d7', // from Okta dashboard
     issuer: 'https://dev-65567071.okta.com/oauth2/default', //dev-65567071.okta.com, Okta domain from okta dashboard
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email'] //scopes provide access to info about a user.
     //openid: required for authentication requests
     //profile: user's first name, last name, phone etc
